d9e67c0739dbdb96a0ef9a286e0b479200177058
Mohammed <atefhamosa@gmail.com>
2021-01-21 17:51:02 +0200

    Update test_spec.rb

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,9 +1,4 @@
 require_relative '../lib/scraper_class.rb'
-require 'httparty'
-require 'nokogiri'
-require 'byebug'
-require 'colorize'
-
 
 # Variables for testing
 link = 'https://deals.souq.com/eg-en/smart-tvs/c/15236'
