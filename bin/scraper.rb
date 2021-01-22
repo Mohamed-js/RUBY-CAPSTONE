81f1f8291739400a538212e02c44da234ff1d543
Mohamed Atef <atefhamosa@gmail.com>
2021-01-22 04:03:46 +0200

    Edit instructions and delete comments

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -7,36 +7,38 @@ scraper = Scraper.new
 
 # WELCOME ::
 puts '******************************************'
+puts ''
 puts 'Welcome to scruby ruby doooooooooooooooooo ^_^'
 puts ''
 puts '******************************************'
-puts '**This version is for testing and reviewing**'
+puts ''
+puts '**Please follow the following instructions:**'
 puts ''
 puts '******************************************'
 # 1-enter Link of page
-puts "Enter the page link 'url' to be scraped:"
+puts "1- Enter the E-commerce page link 'url' to be scraped:"
 link = gets.chomp
-p link
+
 
 # 2-enter SELECTOR of the name of product
-puts 'Enter the page Direct SELECTOR of the name:'
+puts 'Enter the Direct SELECTOR of the name of the product:'
 name = gets.chomp
-p name
+
 
 # 3-enter  SELECTOR of the <a> link of product
-puts "Enter the page Direct SELECTOR of the link 'product page':"
+puts "Enter the Direct SELECTOR of the anchor tag <a> 'product page link':"
 product_page = gets.chomp
-p product_page
+
 
 # 4-enter  SELECTOR of the img of product
-puts 'Enter the page Direct SELECTOR of the img element:'
+puts 'Enter the Direct SELECTOR of the <img> element:'
 img = gets.chomp
-p img
+
 
 # 5-enter  SELECTOR of the price of product
-puts 'Enter the page Direct SELECTOR of the price:'
+puts 'Enter the page Direct SELECTOR of the price containing element:'
 price = gets.chomp
-p price
+
 
 # 6- ASK about img src to set method
 end_loop = false
@@ -48,27 +50,17 @@ while end_loop == false
   end_loop = true if %w[1 2 3].include?(reply)
 end
 
-# ############## un-comment this for production version  ###########
-# # 7- Set the method
-# me = scraper.query(link)
-# goods = scraper.img_in_src(me, name, product_page, img, price) if reply == '1'
-# goods = scraper.img_in_data_src(me, name, product_page, img, price) if reply == '2'
-# if reply == "3"
-#     puts "Enter the attr name:"
-#     the_other_attr = gets.chomp
-#     goods = scraper.img_in_another_attr(me, name, product_page, img, price, the_other_attr)
-# end
-####################################################################
 
-# ############### FOR TESTING ######################################
 # 7- Set the method
-me = scraper.query('https://deals.souq.com/eg-en/smart-tvs/c/15236')
-name = 'h6.title a'
-product_page = 'h6.title a'
-img = 'a.img-link img'
-price = 'h5.price span.is'
-goods = scraper.img_in_data_src(me, name, product_page, img, price)
-###################################################################
+me = scraper.query(link)
+goods = scraper.img_in_src(me, name, product_page, img, price) if reply == '1'
+goods = scraper.img_in_data_src(me, name, product_page, img, price) if reply == '2'
+if reply == "3"
+    puts "Enter the attr name:"
+    the_other_attr = gets.chomp
+    goods = scraper.img_in_another_attr(me, name, product_page, img, price, the_other_attr)
+end
+
 
 # 8- Display Items
 display_goods(goods)
