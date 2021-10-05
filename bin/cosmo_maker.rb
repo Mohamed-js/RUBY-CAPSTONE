6d736a1c2168105d9637d9fb53f0dfc062b76db0
Mohammed <atefhamosa@gmail.com>
2021-10-05 02:06:22 +0200

    U

---
+++ bin/cosmo_maker.rb
@@ -0,0 +1,34 @@
+require 'csv'
+
+cosmetics = []
+
+en_table = CSV.parse(File.read("cosmo en.csv"), headers: true)
+ar_table = CSV.parse(File.read("cosmo ar.csv"), headers: true)
+
+
+    en_table.each do |en|
+        next if en['image'] == 'https://www.yodawy.com/wp-content/uploads/woocommerce-placeholder.png' || en['image'] == 'https://www.yodawy.com/wp-content/themes/mymedi/images/prod_loading.gif'
+        found = false
+        ar_table.each do |ar|
+            if en['image'] == ar['image']
+                cosmetics.push({
+                    name: en['name'],
+                    ar_name: ar['name'],
+                    image: en['image'],
+                    category: en['category'],
+                    sub_category: en['sub_category'],
+                    sub_category_id: en['sub_category_id']
+                })
+            end
+        end
+    end
+
+
+cosmetics
+
+
+CSV.open("cosmo.csv", "a") do |csv|
+    cosmetics.each do |drug|
+        csv << [drug[:name], drug[:ar_name], drug[:image_data], drug[:category], drug[:sub_category], drug[:sub_category_id]]
+    end
+end
--- bin/scraper.rb
+++ bin/scraper.rb
@@ -5,27 +5,39 @@ require_relative '../lib/scraper_class'
 # Initialize the instance
 scraper = Scraper.new
 
+
+# Hair
+links = [['https://www.yodawy.com/shop/hair-care/conditioner/?per_page=200',"Conditioner"], ['https://www.yodawy.com/shop/hair-care/hair-styling/?per_page=200',"Hair Styling"], ['https://www.yodawy.com/shop/hair-care/hair-gel/?per_page=200',"Hair Care"], ['https://www.yodawy.com/shop/hair-care/shampoo/?per_page=200',"Shampoo"], ['https://www.yodawy.com/shop/hair-care/hair-dyes/?per_page=200',"Hair Dye"], ['https://www.yodawy.com/shop/hair-care/anti-hair-loss/?per_page=200',"Hair Loss"], ['https://www.yodawy.com/shop/hair-care/hair-treatments/?per_page=200',"Hair Treatment"]]
+
+# Skin
+# links = ['https://www.yodawy.com/ar/shop/beauty/nail-care/?per_page=200','https://www.yodawy.com/ar/shop/beauty/skin-care/?per_page=200','https://www.yodawy.com/ar/shop/beauty/lip-care/?per_page=200','https://www.yodawy.com/ar/shop/beauty/facial-care/?per_page=200','https://www.yodawy.com/ar/shop/beauty/suncare/?per_page=200','https://www.yodawy.com/ar/shop/beauty/skin-whitening/?per_page=200','https://www.yodawy.com/ar/shop/beauty/acne-treatment/?per_page=200','https://www.yodawy.com/ar/shop/beauty/makeup/?per_page=200',]
+
+# Men Care
+# links = ['https://www.yodawy.com/ar/shop/men-care/?per_page=200', 'https://www.yodawy.com/ar/shop/men-care/page/2/?per_page=200']
+
+# Baby
+# links = ['https://www.yodawy.com/ar/shop/baby/page/1/?per_page=200','https://www.yodawy.com/ar/shop/baby/page/2/?per_page=200','https://www.yodawy.com/ar/shop/baby/page/3/?per_page=200','https://www.yodawy.com/ar/shop/baby/page/4/?per_page=200','https://www.yodawy.com/ar/shop/baby/page/5/?per_page=200','https://www.yodawy.com/ar/shop/baby/page/6/?per_page=200',]
+
+# Women Care
+# links = ['https://www.yodawy.com/ar/shop/women-care/hair-removal/?per_page=200','https://www.yodawy.com/ar/shop/women-care/panty-liners/?per_page=200','https://www.yodawy.com/ar/shop/women-care/sanitary-pads/?per_page=200','https://www.yodawy.com/ar/shop/women-care/womens-deodorants/?per_page=200','https://www.yodawy.com/ar/shop/women-care/woman-shavers/?per_page=200','https://www.yodawy.com/ar/shop/women-care/body-splash-mist/?per_page=200']
+
+# Sexual
+# links = ['https://www.yodawy.com/ar/shop/sexual-wellness/?per_page=200']
+
 # 1-enter Link of page
-puts '1- Enter the product page link to be scraped :'
-link = gets.chomp
-
-puts '2- Enter the category name :'
-category = gets.chomp
-
-puts '3- Enter the category_id :'
-category_id = gets.chomp
-
-
-me = scraper.query(link)
-drugs = scraper.img_in_src(me, 'ًBaby Care', category, category_id)
+# puts '1- Enter the product page link to be scraped :'
+# link = gets.chomp
 
 
 
+links.each_with_index do |link, i|
+    page = scraper.query(link[0])
+    drugs = scraper.img_in_src(page,'Hair Care',link[1], i+1)
 
-# Write the results
-
-CSV.open("cosmo.csv", "w") do |csv|
-    drugs.each do |drug|
-        csv << [drug[:name], drug[:image_data], drug[:category], drug[:sub_category], drug[:cosm_cat_id]]
+    # Write the results
+    CSV.open("cosmo.csv", "a") do |csv|
+        drugs.each do |drug|
+            csv << [drug[:name], drug[:image_data], drug[:category], drug[:sub_category], drug[:cosm_cat_id]]
+        end
     end
 end
